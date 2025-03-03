09c1d8fb192d1500bcf531037321c411d0272013
Anica <anicagabriellips@gmail.com>
2025-03-03 13:20:36 -0300

    ajustes na página inicial

--- js/index.js
+++ js/index.js
@@ -8,14 +8,14 @@ const iframe = document.createElement('iframe');
 // inserindo conteúdo em p e adcionando
 introducao.id = 'introducao';
 
-introducao.innerHTML = "Descubra a força da representatividade feminina! Nosso Projeto Integrador visa destacar e celebrar as mulheres negras líderes em Guanambi. Explore conosco como suas trajetórias e realizações inspiram mudanças e promovem a igualdade de gênero e raça. Junte-se a nós nessa jornada de reconhecimento e visibilidade. Juntas, podemos construir um futuro mais inclusivo e diversificado!";
+introducao.innerHTML = "Toda mulher negra não vivencia suas experiências sozinha. Toda mulher negra brasileira sabe o peso que a sua cor e o seu gênero trazem, sabe as barreiras que, mesmo sem escolher, vai ter que lidar durante a vida, sabe a dureza que é ter que se provar boa o tempo todo e, mesmo assim, não ser o suficiente. Nós, mulheres negras, também sabemos que a mudança não se faz em uma única ação. Sabemos o poder da união e da persistência para transformar a nossa realidade e permitir que possamos ir e vir sem violência e discriminação, sem risco a nossa sobrevivência.  A partir dessa perspectiva, idealizamos esse projeto que buscou essas mulheres na cidade de Guanambi com o intuito de ouví-las e dar maior visibilidade às vivências delas, para que assim mais mulheres possam se reconhecer e fortalecerem a luta feminista e antirracista.";
 
 main.appendChild(introducao);
 
 // inserindo iframe e atributos
 
 iframe.id = 'iframe';
-iframe.setAttribute("height", "260px");
+iframe.setAttribute("height", "300px");
 iframe.setAttribute("src", "https://www.youtube.com/embed/BwCGXExjFyI?si=8aPAVrDOhnJumx9o");
 iframe.setAttribute("title", "YouTube video player");
 iframe.setAttribute("frameborder", "0");
